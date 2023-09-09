07b19c7f4f0d0efd7e927af657e9fb7381180fbe
JB <sjbh3mc@gmail.com>
2023-09-09 13:03:34 +0900

    심준보 : GlobalSingleton 스크립트 추가, 스크립트 summary 에 사용법 존재

---
+++ RockOfAge/Assets/Test/SJB/Scripts/Global_Find.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalUsefulFunctions
+{
+    public static List<int> FindEveryObject(string objectName_, GameObject root_) 
+    {
+        List<int> list = new List<int>();
+
+        //list.Add
+
+
+
+        return list;
+    }
+}
--- RockOfAge/Assets/Test/SJB/Scripts/UI/ButtonManager.cs
+++ RockOfAge/Assets/Test/SJB/Scripts/UI/ButtonManager.cs
@@ -1,25 +1,31 @@
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonManager : MonoBehaviour
+public class ButtonManager : GlobalSingleton<ButtonManager>
 {
     private Button optionButton;
     private Button startButton;
     private Button quitButton;
     
-    private void Awake()
+    protected override void Awake()
     {
         optionButton = GameObject.Find("Button_Option").GetComponent<Button>();
         startButton = GameObject.Find("Button_Start").GetComponent<Button>();
         quitButton = GameObject.Find("Button_Quit").GetComponent<Button>();
     }
 
-    private void OnEnable()
+    private void PressOption() 
     {
-
+        
     }
 
-    private void OnDisable()
+    private void EnterLobby() 
+    {
+        
+    }
+
+    private void QuitGame() 
     {
         
     }

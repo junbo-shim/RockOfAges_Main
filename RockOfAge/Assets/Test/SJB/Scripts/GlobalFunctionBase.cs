399b10cf1cff357ffdefa4f031278ac446f8a39a
JB <sjbh3mc@gmail.com>
2023-09-11 16:12:44 +0900

    심준보 : Playfab sdk 설치, Global Singleton 스크립트의 create 함수 추가(Scene Manger 만들어서 create 로 부를 것), TitleScene UI 수정, Network Manager 생성 및 Playfab 연동 성공

--- RockOfAge/Assets/Test/SJB/Scripts/GlobalFunctionBase.cs
+++ RockOfAge/Assets/Test/SJB/Scripts/GlobalFunctionBase.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class GlobalFunctionBase : MonoBehaviour
+public class GlobalFunctionBase : MonoBehaviourPunCallbacks
 {
     protected virtual void Awake()
     {
--- RockOfAge/Assets/Test/SJB/Scripts/UI/ButtonManager.cs
+++ RockOfAge/Assets/Test/SJB/Scripts/UI/ButtonManager.cs
@@ -1,26 +1,166 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ButtonManager : GlobalSingleton<ButtonManager>
 {
+    private Transform titlePanel;
+
     private Button optionButton;
-    private Button startButton;
+    private Button quickStartButton;
+    private Button loginButton;
+    private Button signupButton;
     private Button quitButton;
+
+    private Button startButton;
+    private Button resetPWButton;
+    private Button closeButton;
+
+    private Button registerButton;
+    private Button closeButton2;
     
     protected override void Awake()
     {
-        optionButton = GameObject.Find("Button_Option").GetComponent<Button>();
-        startButton = GameObject.Find("Button_Start").GetComponent<Button>();
-        quitButton = GameObject.Find("Button_Quit").GetComponent<Button>();
+        titlePanel = GameObject.Find("Panel_Title").transform;
+        FindAllButtons();
+        SetAllButton();
+        MakePanelsDefault();
     }
 
-    private void OnEnable()
+
+
+
+    #region 타이틀 씬의 모든 버튼을 찾아서 저장하는 메서드
+    private void FindAllButtons() 
     {
+        optionButton = titlePanel.Find("Button_Option").GetComponent<Button>();
+        quickStartButton = titlePanel.Find("Button_QuickStart").GetComponent<Button>();
+        loginButton = titlePanel.Find("Button_Login").GetComponent<Button>();
+        signupButton = titlePanel.Find("Button_Signup").GetComponent<Button>();
+        quitButton = titlePanel.Find("Button_Quit").GetComponent<Button>();
 
+        startButton = titlePanel.Find("Panel_Login").Find("Button_Start").GetComponent<Button>();
+        resetPWButton = titlePanel.Find("Panel_Login").Find("Button_ResetPW").GetComponent<Button>();
+        closeButton = titlePanel.Find("Panel_Login").Find("Button_Close").GetComponent<Button>();
+
+        registerButton = titlePanel.Find("Panel_Signup").Find("Button_Register").GetComponent<Button>();
+        closeButton2 = titlePanel.Find("Panel_Signup").Find("Button_Close2").GetComponent<Button>();
     }
+    #endregion
 
-    private void OnDisable()
+
+
+
+    #region 버튼에 AddListener 세팅하는 메서드
+    private void SetAllButton() 
     {
-        
+        optionButton.onClick.AddListener(PressOption);
+        quickStartButton.onClick.AddListener(PressQuickStart);
+        loginButton.onClick.AddListener(PressLogin);
+        signupButton.onClick.AddListener(PressSignup);
+        quitButton.onClick.AddListener(PressQuit);
+
+        startButton.onClick.AddListener(PressStart);
+        resetPWButton.onClick.AddListener(PressResetPW);
+        closeButton.onClick.AddListener(PressClose);
+
+        registerButton.onClick.AddListener(PressRegister);
+        closeButton2.onClick.AddListener(PressClose2);
     }
+    #endregion
+
+
+
+
+    #region 모든 창을 작게 만드는 메서드
+    public void MakePanelsDefault() 
+    {
+        Transform loginPanel = titlePanel.Find("Panel_Login").transform;
+        Transform signupPanel = titlePanel.Find("Panel_Signup").transform;
+
+        loginPanel.localScale = Vector3.zero;
+        signupPanel.localScale = Vector3.zero;
+    }
+    #endregion
+
+
+
+
+
+    #region 버튼 기능 메서드
+    public void PressOption() 
+    {
+        /*Empty For Now*/
+    }
+
+    public void PressQuickStart() 
+    {
+        /*임의의 Auth 부여*/
+    }
+
+    public void PressLogin() 
+    {
+        Transform loginPanel = titlePanel.Find("Panel_Login").transform;
+        loginPanel.localScale = Vector3.one;
+    }
+
+    public void PressSignup() 
+    {
+        Transform signupPanel = titlePanel.Find("Panel_Signup").transform;
+        signupPanel.localScale = Vector3.one;
+    }
+
+    public void PressQuit() 
+    {
+        if (UnityEditor.EditorApplication.isPlaying == true) 
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else 
+        {
+            Application.Quit();
+        }
+    }
+
+    public void PressStart() 
+    {
+        NetworkManager.Instance.Login();
+    }
+
+    public void PressResetPW() 
+    {
+        /**/
+    }
+
+    public void PressRegister() 
+    {
+        NetworkManager.Instance.Register();
+    }
+
+    public void PressClose()
+    {
+        Transform loginPanel = titlePanel.Find("Panel_Login").transform;
+        loginPanel.localScale = Vector3.zero;
+
+        TMP_InputField emailInput = loginPanel.Find("InputField_Email").GetComponent<TMP_InputField>();
+        TMP_InputField passwordInput = loginPanel.Find("InputField_Password").GetComponent<TMP_InputField>();
+
+        emailInput.text = default;
+        passwordInput.text = default;
+    }
+
+    public void PressClose2()
+    {
+        Transform signupPanel = titlePanel.Find("Panel_Signup").transform;
+        signupPanel.localScale = Vector3.zero;
+
+        TMP_InputField emailInput = signupPanel.Find("InputField_Email").GetComponent<TMP_InputField>();
+        TMP_InputField passwordInput = signupPanel.Find("InputField_Password").GetComponent<TMP_InputField>();
+        TMP_InputField nicknameInput = signupPanel.Find("InputField_Nickname").GetComponent<TMP_InputField>();
+
+        emailInput.text = default;
+        passwordInput.text = default;
+        nicknameInput.text = default;
+    }
+    #endregion
 }

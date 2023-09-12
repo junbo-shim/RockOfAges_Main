914b72c7bbcddb85f24de0be303b84a35181c5a4
ZEZE\a0104 <a01049469494@gmail.com>
2023-09-12 15:40:06 +0900

    김재현 : 모루황소 돌진, 돌진후 설치된곳으로 돌아감, 돌 감지중 항상 머리방향이 돌을 향하도록 함.

--- RockOfAge/Assets/Test/KJH/Scripts/KJHBullController.cs
+++ RockOfAge/Assets/Test/KJH/Scripts/KJHBullController.cs
@@ -6,51 +6,126 @@ using System;
 
 public class KJHBullController : MonoBehaviour
 {
-    public float detectionRange = 5.0f;
+    public float detectionRange = default;
     public float chargeSpeed = 10.0f;
     public LayerMask Rock;
 
     public float health = 100f; // 체력 변수
     public float attackPower = default; // 공격력 변수
     public float chargeCool = 5.0f;
-    public Transform headTransform; // 모루황소 머리의 Transform을 참조합니다.
+    public float walkSpeed = 3.0f;
 
+    private Vector3 lastRockPosition; // 돌의 마지막 위치를 저장할 변수 추가
+    private bool hasCharged = false; // 모루황소가 돌진했는지 여부를 나타내는 변수 추가
     private float lastChargeTime = 0f;
     private Rigidbody bullRigidbody;
     private bool isCharging = false;
     private Transform targetRock;
     private Animator animator;
     private int chargeCount = 0;
+    private Vector3 initialBullPosition;
+    private bool isReturning = false;
 
     private void Start() // Start 메서드를 선언합니다.
     {
         animator = GetComponent<Animator>(); // 애니메이터 컴포넌트를 가져옵니다.
         bullRigidbody = GetComponent<Rigidbody>(); // 리지드바디 컴포넌트를 가져옵니다.
-
+        initialBullPosition = transform.position;
     }
 
+
+    //private void Update()
+    //{
+    //    if (!isCharging)
+    //    {
+    //        lastChargeTime += Time.deltaTime;
+    //        if (lastChargeTime >= chargeCool)
+    //        {
+    //            DetectRock();
+    //            if (isCharging)
+    //            {
+    //                lastChargeTime = 0f;
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        ChargeTowardsRock();
+
+    //        float distanceToLastRockPosition = Vector3.Distance(transform.position, lastRockPosition);
+
+    //        // 돌진 중일 때 돌의 마지막 좌표에 도착했는지 확인합니다.
+    //        if (distanceToLastRockPosition <= 10.0f)
+    //        {
+    //            Debug.LogFormat("????");
+    //            ResetCharge();
+    //        }
+    //    }
+    //    // 머리가 돌을 바라보게 합니다.
+    //    if (targetRock != null)
+    //    {
+    //        if (isCharging)
+    //        {
+    //            // 돌진 중일 때는 돌진 방향을 바라봅니다.
+    //            Vector3 chargeDirection = (lastRockPosition - transform.position).normalized;
+    //            chargeDirection.y = 0;
+    //            transform.rotation = Quaternion.LookRotation(chargeDirection);
+    //        }
+    //        else
+    //        {
+    //            // 돌진 중이 아닐 때는 돌을 바라봅니다.
+    //            transform.LookAt(new Vector3(targetRock.transform.position.x, transform.position.y, targetRock.transform.position.z));
+    //        }
+    //    }
+    //}
     private void Update()
     {
-        if (!isCharging)
+        animator.SetBool("isCharging", isCharging);
+        animator.SetBool("isReturning", isReturning);
+
+        if (!isCharging && !isReturning)
         {
             lastChargeTime += Time.deltaTime;
             if (lastChargeTime >= chargeCool)
             {
                 DetectRock();
+                if (isCharging)
+                {
+                    lastChargeTime = 0f;
+                }
             }
         }
-        else
+        else if (isCharging)
         {
             ChargeTowardsRock();
-        }
 
+            float distanceToLastRockPosition = Vector3.Distance(transform.position, lastRockPosition);
+
+            if (distanceToLastRockPosition <= 10.0f)
+            {
+                Debug.LogFormat("????");
+                isCharging = false;
+                isReturning = true;
+            }
+        }
+        else if (isReturning)
+        {
+            ReturnToInitialPositionBackwards();
+
+            float distanceToInitialPosition = Vector3.Distance(transform.position, initialBullPosition);
+
+            if (distanceToInitialPosition <= 1.0f)
+            {
+                ResetCharge();
+                isReturning = false;
+            }
+        }
         // 머리가 돌을 바라보게 합니다.
-        if (targetRock != null)
+        if (targetRock != null && !isCharging && !isReturning)
         {
             transform.LookAt(new Vector3(targetRock.transform.position.x, transform.position.y, targetRock.transform.position.z));
         }
     }
-
     public void TakeDamage(float damage) // 데미지를 받는 메서드를 선언합니다.
     {
         health -= damage; // 체력을 감소시킵니다.
@@ -59,31 +134,33 @@ public class KJHBullController : MonoBehaviour
             Destroy(gameObject); // 게임 오브젝트를 제거합니다.
         }
     }
-
     private void DetectRock() // 돌을 탐지하는 메서드를 선언합니다.
     {
         Collider[] rocks = Physics.OverlapSphere(transform.position, detectionRange, Rock);
-        if (rocks.Length > 0 && rocks[0] != null && chargeCount < 1)
+        if (rocks.Length > 0 && rocks[0] != null)
         {
             targetRock = rocks[0].transform;
-            isCharging = true;
+            lastRockPosition = targetRock.position; // 돌의 현재 위치를 저장
+            isCharging = true; // 돌진 상태를 true로 설정합니다.
         }
     }
-
     private void ChargeTowardsRock() // 돌을 향해 돌진하는 메서드를 선언합니다.
     {
-        Vector3 direction = (targetRock.position - transform.position).normalized;
-        float distanceToTarget = Vector3.Distance(targetRock.position, transform.position);
+        Vector3 direction = (lastRockPosition - transform.position).normalized;
+        direction.y = 0; // y축 값을 0으로 설정합니다.r
+        float distanceToTarget = Vector3.Distance(lastRockPosition, transform.position);
         bullRigidbody.velocity = direction * chargeSpeed;
     }
 
-
-
-    void ResetCharge() // 돌진 상태를 초기화하는 메서드를 선언합니다.
+    void ResetCharge()
     {
         lastChargeTime = 0f;
         chargeCount = 0;
         isCharging = false;
+        if (targetRock != null)
+        {
+            transform.LookAt(new Vector3(targetRock.transform.position.x, transform.position.y, targetRock.transform.position.z));
+        }
     }
     void OnCollisionEnter(Collision collision) // 충돌이 발생했을 때의 메서드를 선언합니다.
     {
@@ -91,7 +168,11 @@ public class KJHBullController : MonoBehaviour
 
         if (rock != null && chargeCount < 1)
         {
+
+            hasCharged = true;
             chargeCount++;
+            isCharging = false; // 충돌 시 돌진 상태를 false로 설정합니다.
+            isReturning = true; // 충돌 후 후퇴 상태를 true로 설정합니다.
             Rigidbody rockRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Rigidbody bullRigidbody = GetComponent<Rigidbody>();
 
@@ -114,4 +195,17 @@ public class KJHBullController : MonoBehaviour
 
     }
 
+    private void ReturnToInitialPositionBackwards()
+    {
+        Vector3 direction = (initialBullPosition - transform.position).normalized;
+        direction.y = 0;
+        transform.rotation = Quaternion.LookRotation(-direction);
+        transform.position += direction * walkSpeed * Time.deltaTime;
+
+        float distanceToInitialPosition = Vector3.Distance(transform.position, initialBullPosition);
+        if (distanceToInitialPosition <= 1.0f)
+        {
+            isReturning = false;
+        }
+    }
 }

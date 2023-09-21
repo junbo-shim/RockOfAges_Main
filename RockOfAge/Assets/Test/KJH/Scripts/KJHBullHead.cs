8bb9501ec212cba3b528ee191cfa75dd79562d2f
ZEZE\a0104 <a01049469494@gmail.com>
2023-09-21 16:25:40 +0900

    김재현 : 모루황소 애니메이션 완성, 콜라이더의 태그로 Die하게 함

--- RockOfAge/Assets/Test/KJH/Scripts/KJHBullHead.cs
+++ RockOfAge/Assets/Test/KJH/Scripts/KJHBullHead.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System.Collections;
-public class KJHBullController : MoveObstacleBase, IHitObjectHandler
+public class KJHBullHead : MoveObstacleBase, IHitObjectHandler
 {
     public float detectionRadius = 5f;     // 바위 감지 범위
     public float detectionAngle = 90f;     // 바위 감지 각도
@@ -14,7 +14,7 @@ public class KJHBullController : MoveObstacleBase, IHitObjectHandler
     private bool isReturning = false;      // 원래 위치로 복귀 중 여부
     private Animator animator; // Animator 컴포넌트 참조
 
-    private float stareTime = 1f; // 바위를 바라보는 시간 (2초)
+    private float stareTime = 0.8f; // 바위를 바라보는 시간 
     private bool isStaring = false; // 바위를 바라보고 있는지 여부
     private float stareTimer = 0f; // 바위를 바라보기 시작한 시간을 추적하는 타이머
 
@@ -45,6 +45,10 @@ public class KJHBullController : MoveObstacleBase, IHitObjectHandler
                     lastDetectedRockPosition = rock.transform.position;
                     isStaring = true;
                     stareTimer = Time.time;
+
+                    // Attack 애니메이션 트리거 리셋
+                    animator.ResetTrigger("Attack");
+
                     break;
                 }
             }
@@ -65,10 +69,10 @@ public class KJHBullController : MoveObstacleBase, IHitObjectHandler
                 isCharging = true;
             }
         }
-        else if (isCharging)
-        {
-            ChargeToLastDetectedRock();
-        }
+       else if (isCharging)
+    {
+        ChargeToLastDetectedRock();
+    }
         else if (isReturning)
         {
             ReturnToOriginalPosition();
@@ -92,32 +96,29 @@ public class KJHBullController : MoveObstacleBase, IHitObjectHandler
         {
             // 돌진 애니메이션 종료
             animator.SetBool("isCharging", false);
-
-            animator.SetTrigger("Attack");
-
+            
             Debug.Log("바위의 마지막 위치로 돌진 완료");
-            isCharging = false;
-            isReturning = true;
+            Invoke("Wait", 1f);
         }
     }
    
     void ReturnToOriginalPosition()
     {
-        animator.SetBool("isReturning", true);
         Vector3 direction = (originalPosition - transform.position).normalized;
         direction.y = 0; // Y 축 이동 금지
 
         // 원래 위치로 복귀
         transform.position += direction * returnSpeed * Time.deltaTime;
+        animator.SetBool("isReturning", true);
 
         // 복귀 완료 여부 확인
         if (Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(originalPosition.x, 0, originalPosition.z)) < 0.1f)
         {
             // 돌진 애니메이션 종료
-            animator.SetBool("isReturning", false);
 
             Debug.Log("원래 위치로 복귀 완료");
             isReturning = false;
+            animator.SetBool("isReturning", false);
         }
     }
 
@@ -127,8 +128,8 @@ public class KJHBullController : MoveObstacleBase, IHitObjectHandler
         {
             Debug.Log("바위와 충돌: " + collision.gameObject.name);
 
+
             // 공격 애니메이션 시작
-            animator.SetTrigger("Attack");
             // 충돌한 바위에 힘을 전달
             Rigidbody rockRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             if (rockRigidbody != null)
@@ -137,25 +138,72 @@ public class KJHBullController : MoveObstacleBase, IHitObjectHandler
                 float forceMagnitude = GetComponent<Rigidbody>().mass * chargeSpeed;
                 rockRigidbody.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
             }
+            Debug.Log(collision.contacts[0].thisCollider);
+           
+                // 몸통이 아닌 다른 부분에 충돌했을 때의 처리
+                Debug.Log("어딘가에 충돌");
+                animator.SetTrigger("Attack");
+            
         }
-    }
-
-    void OnDrawGizmos()
-    {
-        // 부채꼴 감지 범위를 그립니다.
-        Gizmos.color = Color.blue;
-        float step = detectionAngle / 20; // 부채꼴을 20개의 선분으로 나눕니다.
-        for (float angle = -detectionAngle * 0.5f; angle <= detectionAngle * 0.5f; angle += step)
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Rock"))
         {
-            Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
-            Gizmos.DrawLine(transform.position, transform.position + direction * detectionRadius);
-        }
+            Debug.Log("바위와 충돌: " + collision.gameObject.name);
 
-        // 원형 감지 범위를 그립니다.
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+
+          
+            Debug.Log(collision.contacts[0].thisCollider);
+            // 충돌한 콜라이더가 몸통인지 확인
+            if (collision.contacts[0].thisCollider.CompareTag("BullBody"))
+            {
+                // 몸통에 충돌했을 때 모루 황소를 죽이는 코드를 여기에 추가하세요.
+                Debug.Log("몸통에 충돌");
+                Die();
+            }
+            else
+            {
+                // 몸통이 아닌 다른 부분에 충돌했을 때의 처리
+                Debug.Log("어딘가에 충돌");
+                animator.SetTrigger("Attack");
+            }
+        }
     }
 
+    //void OnDrawGizmos()
+    //{
+    //    // 부채꼴 감지 범위를 그립니다.
+    //    Gizmos.color = Color.blue;
+    //    float step = detectionAngle / 20; // 부채꼴을 20개의 선분으로 나눕니다.
+    //    for (float angle = -detectionAngle * 0.5f; angle <= detectionAngle * 0.5f; angle += step)
+    //    {
+    //        Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+    //        Gizmos.DrawLine(transform.position, transform.position + direction * detectionRadius);
+    //    }
+
+    //    // 원형 감지 범위를 그립니다.
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    //}
+    public void Wait()
+    {
+        isCharging = false;
+        isReturning = true;
+
+    }
+    private void Die()
+    {
+        // Die 애니메이션 재생
+        animator.SetTrigger("Die");
+
+        // 1초 후에 사라지는 로직을 실행
+        Invoke("Disappear", 2f);
+    }
+
+    private void Disappear()
+    {
+        // 게임 오브젝트를 비활성화하거나 파괴
+        gameObject.SetActive(false);
+        // 또는 Destroy(gameObject);
+    }
     public void Hit(int damage)
     {
         // 아직 구현되지 않음
